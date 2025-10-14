257df1a72abb5e79b2b63041e9002195d1c1e140
Diogo-Barboza <diogorodriguesbb@gmail.com>
2025-10-13 21:06:18 -0300

    first commit

---
+++ tppecampeonatobrasileiro/src/main/java/tppe/campeonato/App.java
@@ -0,0 +1,13 @@
+package tppe.campeonato;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
